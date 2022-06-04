8ff7c4f6bae95d8e989148d34a11519b03be0416
Daniel Bramer <Daniel.Bramer@kuka.com>
2022-06-04 23:05:28 +0200

    Clean up

--- src/FluentSetups.SourceGenerator/FluentSetupDiagnostics.cs
+++ src/FluentSetups.SourceGenerator/FluentSetupDiagnostics.cs
@@ -6,13 +6,16 @@
 
 namespace FluentSetups.SourceGenerator
 {
+   using System.Diagnostics.CodeAnalysis;
+
    using Microsoft.CodeAnalysis;
 
-   public class FluentSetupDiagnostics
+   [SuppressMessage("MicrosoftCodeAnalysisReleaseTracking", "RS2008:Enable analyzer release tracking")]
+   public static class FluentSetupDiagnostics
    {
       internal static readonly DiagnosticDescriptor MultiplePartialParts = new DiagnosticDescriptor(id: "FSI0001",
          title: "FluentSetups source generator",
-         messageFormat: "Fluent setup generation for class {0} is skipped du tue multiple partial members",
+         messageFormat: "Fluent setup generation for class '{0}' is skipped du tue multiple partial members",
          category: "FluentSetups",
          defaultSeverity: DiagnosticSeverity.Info,
          isEnabledByDefault: true);
